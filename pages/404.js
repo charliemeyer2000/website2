329675bbe7dd39beb828d7e918129d6d87ff4849
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-08-14 22:43:00 -0400

    better

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,7 @@ export default function Custom404() {
   return (
     <Page description="404 - Page not found" title="404">
       <h1>404 - Not Found</h1>
-      <p style={{ marginTop: "var(--gap-double)" }}>
+      <p style={{ marginTop: "var(--gap-double)", lineHeight: 'var(--title)' }}>
         Did you know that the 404 error page originates from a room number at
         CERN?
       </p>
