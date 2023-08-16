ccfa1f1ed6148dd0d609512b6690f4bc8c656bc3
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-08-15 22:14:09 -0400

    youtube, two pieces

---
+++ components/youTubeVideo/YouTubeVideo.js
@@ -0,0 +1,13 @@
+export default function YouTubeVideo({ src, autoplay = false }) {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={autoplay ? `${src}?autoplay=1` : src}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  );
+}
--- static/types/Components.js
+++ static/types/Components.js
@@ -2,13 +2,14 @@
 import styles from "./Components.module.scss";
 import Link from "@/components/link/Link";
 import BackTicks from "@/components/backTicks/BackTicks";
+import YouTubeVideo from "@/components/youTubeVideo/YouTubeVideo";
 const components = {
   h1: (props) => <h1 className={styles.h1} {...props} />,
   h2: (props) => <h2 className={styles.h2} {...props} />,
   p: (props) => <p className={styles.p} {...props} />,
   a: (props) => <Link {...props} />,
-  BackTicks
-  /* make backticks look like code */
+  BackTicks,
+  YouTubeVideo: (props) => <YouTubeVideo {...props} />,
 };
 
 export default components;
