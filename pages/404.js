8c7889cd46663b9858450e42c8a24e6cd799113d
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-08-28 09:08:13 -0400

    removed a comment to test webhooks

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,10 @@
 import Page from "@/components/page/Page";
-import { useEffect } from "react";
 
 export default function Custom404() {
-  // useEffect(() => {
-  //   const unsubscribe = window.addEventListener("click", () => {
-  //     window.history.back();
-  //   });
-
-  //   return unsubscribe;
-  // }, []);
-
-
   return (
     <Page description="404 - Page not found" title="404">
       <h1>404 - Not Found</h1>
-      <p style={{ marginTop: "var(--gap-double)", lineHeight: 'var(--title)' }}>
+      <p style={{ marginTop: "var(--gap-double)", lineHeight: "var(--title)" }}>
         Did you know that the 404 error page originates from a room number at
         CERN?
       </p>
